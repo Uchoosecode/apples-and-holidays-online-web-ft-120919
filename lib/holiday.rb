6a0137fde75cd634ce6731acc417abf56de90e2e
Ayako Wiltshire <awittyl44@gmail.com>
2019-12-18 11:04:26 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -69,13 +69,7 @@ def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
   holiday_bbq = []
-  holiday_hash.collect do |season, holidays|
-    holidays
-   
-binding.pry 
-# end
-return holiday_bbq
-end
+
 end
 
 
